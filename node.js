c7d7dd49e856409c07bbf826f1011e639a0c6310
MitchelvanBever <mitchel@storyofams.com>
2020-08-12 09:33:28 +0200

    fix(config): 🐛  config is now split up
    we also have a very minimal base implementation for typescript projects

--- node.js
+++ node.js
@@ -1,3 +1,115 @@
-const eslintrc = require('./.eslintrc-node');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@module', './src/modules'],
+          ['@common', './src/common'],
+          ['@util', './src/util'],
+        ],
+        extensions: ['.js', '.ts'],
+      },
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-member-accessibility': [1],
+    '@typescript-eslint/interface-name-prefix': [0, 'never'],
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-namespace': 0,
+    '@typescript-eslint/no-inferrable-types': 0,
+    'import/extensions': [
+      1,
+      'never',
+      {
+        svg: 'always',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
+    'no-use-before-define': 0,
+    'no-console': 0,
 
-module.exports = eslintrc;
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          'builtin',
+          'external',
+          ['internal', 'parent'],
+          ['sibling', 'index'],
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@**/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: [],
+      },
+    ],
+
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'none',
+        useTabs: false,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
