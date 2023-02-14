940da7b58171adc98f45b35339e695b840f36859
Mitchel van Bever <mitchel@instantcommerce.io>
2023-02-14 19:04:15 +0100

    fix(prettier): useTabs: true 🍿

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 module.exports = {
-  extends: ['prettier'],
-  plugins: ['@typescript-eslint', 'import', 'prettier'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts'],
-      },
-    },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts'],
-    },
-  },
-  rules: {
-    // come up with some nice rules here
+	extends: ['prettier'],
+	plugins: ['@typescript-eslint', 'import', 'prettier'],
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.ts'],
+			},
+		},
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts'],
+		},
+	},
+	rules: {
+		// come up with some nice rules here
 
-    'import/order': [
-      'error',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        groups: ['builtin', 'external', ['internal', 'parent'], ['sibling', 'index']],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '~**',
-            group: 'internal',
-            position: 'before',
-          },
-          {
-            pattern: '~**/**',
-            group: 'internal',
-            position: 'before',
-          },
-        ],
-        pathGroupsExcludedImportTypes: [],
-      },
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 100,
-        semi: true,
-        singleQuote: true,
-        tabWidth: 2,
-        trailingComma: 'all',
-        useTabs: false,
-      },
-    ],
-  },
+		'import/order': [
+			'error',
+			{
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+				groups: ['builtin', 'external', ['internal', 'parent'], ['sibling', 'index']],
+				pathGroups: [
+					{
+						pattern: 'react',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: '~**',
+						group: 'internal',
+						position: 'before',
+					},
+					{
+						pattern: '~**/**',
+						group: 'internal',
+						position: 'before',
+					},
+				],
+				pathGroupsExcludedImportTypes: [],
+			},
+		],
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 100,
+				semi: true,
+				singleQuote: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				useTabs: true,
+			},
+		],
+	},
 };
